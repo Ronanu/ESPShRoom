c9b3710a3f2bdfb4e1d4cf60ba5b3194cce4de87
Fx <felixrodlsberger@gmail.com>
2024-09-02 13:30:21 +0200

    Revert "all works, but no sensor vals."
    This reverts commit 6f489555797efc159f6ba10cc9c2e17ac10fbdb8.

--- ESP_ShRoom/src/main.cpp
+++ ESP_ShRoom/src/main.cpp
@@ -1,29 +1,95 @@
-#include <Wire.h>
-#include "DFRobot_SHT20.h"
+#include <WiFi.h>
+#include <WebServer.h>
 
-DFRobot_SHT20 sht20;
+// Dummy-Werte für Sensoren
+float temperature1 = 25.3;
+float temperature2 = 26.7;
+float humidity1 = 45.8;
 
-void setup()
-{
-    Serial.begin(9600);
-    Wire.begin(21, 22);  // Verwende Standard SDA und SCL Pins
-    Wire.setClock(100000); // Reduziere die I²C-Geschwindigkeit auf 100kHz
+// Lüftereinschaltdauer (in Prozent)
+int fan1DutyCycle = 50;  // Standardwert 50%
+int fan2DutyCycle = 50;  // Standardwert 50%
 
-    Serial.println("SHT20 Example!");
-    sht20.initSHT20();                         // Init SHT20 Sensor
-    delay(100);
-    sht20.checkSHT20();                        // Check SHT20 Sensor
+// Soll-Temperatur (in °C)
+float targetTemperature = 22.0;  // Standardwert 22°C
+
+const char* ssid = "ESP32_AP";
+const char* password = "12345678";
+
+WebServer server(80);
+
+void handleRoot() {
+    String html = "<html><body>";
+    html += "<h1>ESP32 Steuerung</h1>";
+    html += "<form action=\"/set_values\" method=\"GET\">";
+    
+    // Eingabefelder für Lüftereinschaltdauer in Prozent
+    html += "Einschaltdauer L&uuml;fter 1 (in Prozent): ";
+    html += "<input type=\"number\" name=\"fan1\" value=\"" + String(fan1DutyCycle) + "\" min=\"0\" max=\"100\"><br>";
+    html += "Einschaltdauer L&uuml;fter 2 (in Prozent): ";
+    html += "<input type=\"number\" name=\"fan2\" value=\"" + String(fan2DutyCycle) + "\" min=\"0\" max=\"100\"><br>";
+    
+    // Eingabefeld für Soll-Temperatur
+    html += "Soll-Temperatur (in &deg;C): ";
+    html += "<input type=\"number\" name=\"targetTemp\" value=\"" + String(targetTemperature) + "\" step=\"0.1\"><br>";
+    
+    html += "<input type=\"submit\" value=\"OK\">";
+    html += "</form>";
+
+    // Ausgabe der aktuellen Sensordaten
+    html += "<h2>Sensordaten</h2>";
+    html += "Temperatur 1: " + String(temperature1) + " &deg;C<br>";
+    html += "Temperatur 2: " + String(temperature2) + " &deg;C<br>";
+    html += "Luftfeuchtigkeit 1: " + String(humidity1) + " %<br>";
+    
+    html += "</body></html>";
+
+    server.send(200, "text/html", html);
 }
 
-void loop()
-{
-    float humd = sht20.readHumidity();         // Read Humidity
-    float temp = sht20.readTemperature();      // Read Temperature
-    Serial.print(" Temperature: ");
-    Serial.print(temp, 1);
-    Serial.print("C");
-    Serial.print("\t Humidity: ");
-    Serial.print(humd, 1);
-    Serial.println("%");
-    delay(1000);
+void handleSetValues() {
+    if (server.hasArg("fan1")) {
+        fan1DutyCycle = server.arg("fan1").toInt();
+    }
+
+    if (server.hasArg("fan2")) {
+        fan2DutyCycle = server.arg("fan2").toInt();
+    }
+
+    if (server.hasArg("targetTemp")) {
+        targetTemperature = server.arg("targetTemp").toFloat();
+    }
+
+    String html = "<html><body>";
+    html += "<h1>Werte aktualisiert</h1>";
+    html += "<p>Einschaltdauer L&uuml;fter 1: " + String(fan1DutyCycle) + " %</p>";
+    html += "<p>Einschaltdauer L&uuml;fter 2: " + String(fan2DutyCycle) + " %</p>";
+    html += "<p>Soll-Temperatur: " + String(targetTemperature) + " &deg;C</p>";
+    html += "<a href=\"/\">Zur&uuml;ck</a>";
+    html += "</body></html>";
+
+    server.send(200, "text/html", html);
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    // Konfiguration des ESP32 als Access Point
+    WiFi.softAP(ssid, password);
+    Serial.println("Access Point gestartet");
+    Serial.print("IP-Adresse: ");
+    Serial.println(WiFi.softAPIP());
+
+    // Webserver Routen
+    server.on("/", handleRoot);
+    server.on("/set_values", handleSetValues);
+    server.begin();
+    Serial.println("Webserver gestartet");
+}
+
+void loop() {
+    // Hier würde man normalerweise die Sensordaten aktualisieren
+    // Für dieses Beispiel nehmen wir feste Dummy-Werte an
+
+    server.handleClient();
 }
