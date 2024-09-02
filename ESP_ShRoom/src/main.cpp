fff407d84b6d55a49ec89277196a9873a92e749f
Fx <felixrodlsberger@gmail.com>
2024-09-02 16:10:15 +0200

    basicinput

--- ESP_ShRoom/src/main.cpp
+++ ESP_ShRoom/src/main.cpp
@@ -1,123 +1,70 @@
-#include <Wire.h>
-#include "uFire_SHT20.h"
+#include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
 
-// Sensor-Instanzen
-uFire_SHT20 sht20_1;  // Sensor 1 an Pin 21, 22
-uFire_SHT20 sht20_2;  // Sensor 2 an Pin 18, 19
 
-// Sensordaten
-float temperature1;
-float temperature2;
-float humidity1;
-float humidity2;
-
-// Lüftereinschaltdauer (in Prozent)
-int fan1DutyCycle = 50;  // Standardwert 50%
-int fan2DutyCycle = 50;  // Standardwert 50%
-
-// Soll-Temperatur (in °C)
-float targetTemperature = 22.0;  // Standardwert 22°C
-
-const char* ssid = "ESP32_AP";
-const char* password = "12345678";
-
+// Webserver auf Port 80
 WebServer server(80);
 
+// Initiale Zeit (nur Stunden und Minuten)
+int hours = 0;
+int minutes = 0;
+unsigned long lastUpdateTime = 0; // Variable zum Speichern der letzten Aktualisierungszeit
+
+// Funktion für die Startseite
 void handleRoot() {
-    String html = "<html><body>";
-    html += "<h1>ESP32 Steuerung</h1>";
-    html += "<form action=\"/set_values\" method=\"GET\">";
-    
-    // Eingabefelder für Lüftereinschaltdauer in Prozent
-    html += "Einschaltdauer L&uuml;fter 1 (in Prozent): ";
-    html += "<input type=\"number\" name=\"fan1\" value=\"" + String(fan1DutyCycle) + "\" min=\"0\" max=\"100\"><br>";
-    html += "Einschaltdauer L&uuml;fter 2 (in Prozent): ";
-    html += "<input type=\"number\" name=\"fan2\" value=\"" + String(fan2DutyCycle) + "\" min=\"0\" max=\"100\"><br>";
-    
-    // Eingabefeld für Soll-Temperatur
-    html += "Soll-Temperatur (in &deg;C): ";
-    html += "<input type=\"number\" name=\"targetTemp\" value=\"" + String(targetTemperature) + "\" step=\"0.1\"><br>";
-    
-    html += "<input type=\"submit\" value=\"OK\">";
-    html += "</form>";
-
-    // Ausgabe der aktuellen Sensordaten
-    html += "<h2>Sensordaten</h2>";
-    html += "Temperatur 1: " + String(temperature1, 1) + " &deg;C<br>";
-    html += "Temperatur 2: " + String(temperature2, 1) + " &deg;C<br>";
-    html += "Luftfeuchtigkeit 1: " + String(humidity1, 1) + " %<br>";
-    html += "Luftfeuchtigkeit 2: " + String(humidity2, 1) + " %<br>";
-    
-    html += "</body></html>";
-
-    server.send(200, "text/html", html);
+  String html = "<html><head><meta http-equiv='refresh' content='1'></head><body>";
+  html += "<h1>Aktuelle Uhrzeit</h1>";
+  html += "Stunde: " + String(hours) + "<br>";
+  html += "Minute: " + String(minutes) + "<br>";
+  html += "<form action=\"/setTime\" method=\"POST\">";
+  html += "Stunde: <input type=\"text\" name=\"hour\" value=\"" + String(hours) + "\"><br>";
+  html += "Minute: <input type=\"text\" name=\"minute\" value=\"" + String(minutes) + "\"><br>";
+  html += "<input type=\"submit\" value=\"Setze Uhrzeit\">";
+  html += "</form>";
+  html += "</body></html>";
+  server.send(200, "text/html", html);
 }
 
-void handleSetValues() {
-    if (server.hasArg("fan1")) {
-        fan1DutyCycle = server.arg("fan1").toInt();
-    }
-
-    if (server.hasArg("fan2")) {
-        fan2DutyCycle = server.arg("fan2").toInt();
-    }
-
-    if (server.hasArg("targetTemp")) {
-        targetTemperature = server.arg("targetTemp").toFloat();
-    }
-
-    String html = "<html><body>";
-    html += "<h1>Werte aktualisiert</h1>";
-    html += "<p>Einschaltdauer L&uuml;fter 1: " + String(fan1DutyCycle) + " %</p>";
-    html += "<p>Einschaltdauer L&uuml;fter 2: " + String(fan2DutyCycle) + " %</p>";
-    html += "<p>Soll-Temperatur: " + String(targetTemperature) + " &deg;C</p>";
-    html += "<a href=\"/\">Zur&uuml;ck</a>";
-    html += "</body></html>";
-
-    server.send(200, "text/html", html);
+// Funktion, um die Uhrzeit zu setzen
+void handleSetTime() {
+  if (server.method() == HTTP_POST) {
+    hours = server.arg("hour").toInt();
+    minutes = server.arg("minute").toInt();
+    lastUpdateTime = millis(); // Startzeitpunkt speichern
+    server.send(200, "text/html", "Uhrzeit gesetzt! <a href=\"/\">Zurück</a>");
+  } else {
+    server.send(405, "text/html", "Methode nicht erlaubt!");
+  }
 }
+
+// Funktion zur Aktualisierung der Zeit
+void updateTime() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastUpdateTime >= 60000) { // Eine Minute vergangen?
+    lastUpdateTime = currentMillis;
+    minutes++;
+    if (minutes >= 60) {
+      minutes = 0;
+      hours++;
+      if (hours >= 24) {
+        hours = 0;
+      }
+    }
+  }
+}
+
 
 void setup() {
-    Serial.begin(115200);
-
-    // Initialisierung der Sensoren
-    Wire.begin(21, 22);  // SDA, SCL für Sensor 1
-    if (!sht20_1.begin()) {
-        Serial.println("Sensor 1 nicht gefunden, überprüfe die Verkabelung!");
-        while (1);
-    }
-
-    Wire.begin(18, 19);  // SDA, SCL für Sensor 2
-    if (!sht20_2.begin()) {
-        Serial.println("Sensor 2 nicht gefunden, überprüfe die Verkabelung!");
-        while (1);
-    }
-
-    // Konfiguration des ESP32 als Access Point
-    WiFi.softAP(ssid, password);
-    Serial.println("Access Point gestartet");
-    Serial.print("IP-Adresse: ");
-    Serial.println(WiFi.softAPIP());
-
-    // Webserver Routen
-    server.on("/", handleRoot);
-    server.on("/set_values", handleSetValues);
-    server.begin();
-    Serial.println("Webserver gestartet");
+  Serial.begin(115200);
+  WiFi.softAP("ESP32-AP", "12345678"); // Zugangspunkt (AP) erstellen
+  server.on("/", handleRoot); // Route für die Startseite
+  server.on("/setTime", handleSetTime); // Route für die Zeit setzen
+  server.begin(); // Webserver starten
 }
 
 void loop() {
-    // Sensordaten aktualisieren
-    Wire.begin(21, 22);  // Wechsel zu Sensor 1
-    temperature1 = sht20_1.temperature();
-    humidity1 = sht20_1.humidity();
-
-    Wire.begin(18, 19);  // Wechsel zu Sensor 2
-    temperature2 = sht20_2.temperature();
-    humidity2 = sht20_2.humidity();
-
-    // Webserver anfragen bearbeiten
-    server.handleClient();
+  server.handleClient(); // Eingehende Anfragen verarbeiten
+  updateTime(); // Aktualisiert die Zeit
 }
+
