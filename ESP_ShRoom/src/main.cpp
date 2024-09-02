3d134bf86af427b232aae86a0d78bec9eeca73ba
Fx <felixrodlsberger@gmail.com>
2024-09-02 13:22:27 +0200

    base sensor test1

--- ESP_ShRoom/src/main.cpp
+++ ESP_ShRoom/src/main.cpp
@@ -1,118 +1,29 @@
 #include <Wire.h>
-#include <WiFi.h>
-#include <WebServer.h>
-#include <SHT2x.h>
+#include "DFRobot_SHT20.h"
 
-// Instanzen für zwei SHT20-Sensoren
-SHT2x sht20_1;
-SHT2x sht20_2;
+DFRobot_SHT20 sht20;
 
-// Sensordaten
-float temperature1 = 0.0;
-float humidity1 = 0.0;
-float temperature2 = 0.0;
-float humidity2 = 0.0;
+void setup()
+{
+    Serial.begin(9600);
+    Wire.begin(21, 22);  // Verwende Standard SDA und SCL Pins
+    Wire.setClock(100000); // Reduziere die I²C-Geschwindigkeit auf 100kHz
 
-// Lüftereinschaltdauer (in Prozent)
-int fan1DutyCycle = 50;
-int fan2DutyCycle = 50;
-
-// Soll-Temperatur (in °C)
-float targetTemperature = 22.0;
-
-const char* ssid = "ESP32_AP";
-const char* password = "12345678";
-
-WebServer server(80);
-
-void handleRoot() {
-    String html = "<html><body>";
-    html += "<h1>ESP32 Steuerung</h1>";
-    html += "<form action=\"/set_values\" method=\"GET\">";
-    
-    // Eingabefelder für Lüftereinschaltdauer in Prozent
-    html += "Einschaltdauer L&uuml;fter 1 (in Prozent): ";
-    html += "<input type=\"number\" name=\"fan1\" value=\"" + String(fan1DutyCycle) + "\" min=\"0\" max=\"100\"><br>";
-    html += "Einschaltdauer L&uuml;fter 2 (in Prozent): ";
-    html += "<input type=\"number\" name=\"fan2\" value=\"" + String(fan2DutyCycle) + "\" min=\"0\" max=\"100\"><br>";
-    
-    // Eingabefeld für Soll-Temperatur
-    html += "Soll-Temperatur (in &deg;C): ";
-    html += "<input type=\"number\" name=\"targetTemp\" value=\"" + String(targetTemperature) + "\" step=\"0.1\"><br>";
-    
-    html += "<input type=\"submit\" value=\"OK\">";
-    html += "</form>";
-
-    // Ausgabe der aktuellen Sensordaten
-    html += "<h2>Sensordaten</h2>";
-    html += "Temperatur 1: " + String(temperature1) + " &deg;C<br>";
-    html += "Luftfeuchtigkeit 1: " + String(humidity1) + " %<br>";
-    html += "Temperatur 2: " + String(temperature2) + " &deg;C<br>";
-    html += "Luftfeuchtigkeit 2: " + String(humidity2) + " %<br>";
-    
-    html += "</body></html>";
-
-    server.send(200, "text/html", html);
+    Serial.println("SHT20 Example!");
+    sht20.initSHT20();                         // Init SHT20 Sensor
+    delay(100);
+    sht20.checkSHT20();                        // Check SHT20 Sensor
 }
 
-void handleSetValues() {
-    if (server.hasArg("fan1")) {
-        fan1DutyCycle = server.arg("fan1").toInt();
-    }
-
-    if (server.hasArg("fan2")) {
-        fan2DutyCycle = server.arg("fan2").toInt();
-    }
-
-    if (server.hasArg("targetTemp")) {
-        targetTemperature = server.arg("targetTemp").toFloat();
-    }
-
-    String html = "<html><body>";
-    html += "<h1>Werte aktualisiert</h1>";
-    html += "<p>Einschaltdauer L&uuml;fter 1: " + String(fan1DutyCycle) + " %</p>";
-    html += "<p>Einschaltdauer L&uuml;fter 2: " + String(fan2DutyCycle) + " %</p>";
-    html += "<p>Soll-Temperatur: " + String(targetTemperature) + " &deg;C</p>";
-    html += "<a href=\"/\">Zur&uuml;ck</a>";
-    html += "</body></html>";
-
-    server.send(200, "text/html", html);
-}
-
-void setup() {
-    Serial.begin(115200);
-
-    // WiFi Setup
-    WiFi.softAP(ssid, password);
-    Serial.println("Access Point gestartet");
-    Serial.print("IP-Adresse: ");
-    Serial.println(WiFi.softAPIP());
-
-    // I2C-Initialisierung für den ersten Sensor
-    Wire.begin(21, 22); // SDA = GPIO 21, SCL = GPIO 22
-    sht20_1.begin();
-
-    // I2C-Initialisierung für den zweiten Sensor
-    Wire.begin(18, 19); // SDA = GPIO 18, SCL = GPIO 19
-    sht20_2.begin();
-
-    // Webserver Routen
-    server.on("/", handleRoot);
-    server.on("/set_values", handleSetValues);
-    server.begin();
-    Serial.println("Webserver gestartet");
-}
-
-void loop() {
-    // Sensordaten vom ersten SHT20-Sensor aktualisieren
-    Wire.begin(21, 22); // Erste I2C-Verbindung
-    temperature1 = sht20_1.readTemperature();
-    humidity1 = sht20_1.readHumidity();
-
-    // Sensordaten vom zweiten SHT20-Sensor aktualisieren
-    Wire.begin(18, 19); // Zweite I2C-Verbindung
-    temperature2 = sht20_2.readTemperature();
-    humidity2 = sht20_2.readHumidity();
-
-    server.handleClient();
+void loop()
+{
+    float humd = sht20.readHumidity();         // Read Humidity
+    float temp = sht20.readTemperature();      // Read Temperature
+    Serial.print(" Temperature: ");
+    Serial.print(temp, 1);
+    Serial.print("C");
+    Serial.print("\t Humidity: ");
+    Serial.print(humd, 1);
+    Serial.println("%");
+    delay(1000);
 }
