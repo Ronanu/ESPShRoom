07b6eaa1f4706abb485715543554ea652a10ccef
Fx <felixrodlsberger@gmail.com>
2024-09-02 16:39:57 +0200

    ohne lag bei update

--- ESP_ShRoom/src/main.cpp
+++ ESP_ShRoom/src/main.cpp
@@ -32,19 +32,36 @@ void updateTime() {
 
 // Funktion für die Startseite
 void handleRoot() {
-  String html = "<html><head><meta http-equiv='refresh' content='1'></head><body style='background-color:black; color:white;'>";
+  String html = "<html><body>";
   html += "<h1>Aktuelle Uhrzeit</h1>";
-  html += "Stunde: " + String(hours) + "<br>";
-  html += "Minute: " + String(minutes) + "<br>";
-  html += "Sekunde: " + String(seconds) + "<br>";
+  html += "<div id='time'>Laden...</div>";
   html += "<a href=\"/setTime\"><button>Uhrzeit einstellen</button></a>";
+  html += "<script>";
+  html += "function updateClock() {";
+  html += "  var xhr = new XMLHttpRequest();";
+  html += "  xhr.onreadystatechange = function() {";
+  html += "    if (xhr.readyState == 4 && xhr.status == 200) {";
+  html += "      document.getElementById('time').innerHTML = xhr.responseText;";
+  html += "    }";
+  html += "  };";
+  html += "  xhr.open('GET', '/time', true);";
+  html += "  xhr.send();";
+  html += "}";
+  html += "setInterval(updateClock, 1000);";
+  html += "</script>";
   html += "</body></html>";
   server.send(200, "text/html", html);
 }
 
+// Funktion, um die Uhrzeit im JSON-Format zu senden
+void handleTime() {
+  String time = String(hours) + ":" + String(minutes) + ":" + String(seconds);
+  server.send(200, "text/plain", time);
+}
+
 // Funktion für die Uhrzeit Einstellungsseite
 void handleSetTimePage() {
-  String html = "<html><body style='background-color:black; color:white;'>";
+  String html = "<html><body>";
   html += "<h1>Uhrzeit einstellen</h1>";
   html += "<form action=\"/updateTime\" method=\"POST\">";
   html += "Stunde: <input type=\"text\" name=\"hour\" value=\"" + String(hours) + "\"><br>";
@@ -73,6 +90,7 @@ void setup() {
   Serial.begin(115200);
   WiFi.softAP("ESP32-AP", "12345678"); // Zugangspunkt (AP) erstellen
   server.on("/", handleRoot); // Route für die Startseite
+  server.on("/time", handleTime); // Route für die aktuelle Uhrzeit
   server.on("/setTime", handleSetTimePage); // Route für die Zeit setzen Seite
   server.on("/updateTime", handleSetTime); // Route um die Zeit zu aktualisieren
   server.begin(); // Webserver starten
