7a6f6ce169189ffa737d5fd24cb1499e9bf2b2a3
Fx <felixrodlsberger@gmail.com>
2024-09-07 16:51:33 +0200

    fans work perfect

--- ESP_ShRoom/src/main.cpp
+++ ESP_ShRoom/src/main.cpp
@@ -29,17 +29,17 @@ FanControl* fanControl4;
 void setup() {
     // Beispiel-Settings für Lüfter 1
     settings.isEnabled1 = true;
-    settings.onTime1 = 60;         // 60 Sekunden Zykluszeit
+    settings.onTime1 = 1;         // 60 Sekunden Zykluszeit
     settings.onPercentage1 = 50;   // 50% Einschaltdauer
 
     // Beispiel-Settings für Lüfter 2
     settings.isEnabled2 = true;
-    settings.onTime2 = 60;         // 60 Sekunden Zykluszeit
+    settings.onTime2 = 6;         // 60 Sekunden Zykluszeit
     settings.onPercentage2 = 40;   // 40% Einschaltdauer
 
     // Beispiel-Settings für Lüfter 3
     settings.isEnabled3 = true;
-    settings.onTime3 = 60;         // 60 Sekunden Zykluszeit
+    settings.onTime3 = 5;         // 60 Sekunden Zykluszeit
     settings.onPercentage3 = 60;   // 60% Einschaltdauer
 
     // Beispiel-Settings für Lüfter 4
@@ -48,10 +48,10 @@ void setup() {
     settings.onPercentage4 = 30;   // 30% Einschaltdauer
 
     // FanControl-Instanzen erstellen und initialisieren
-    fanControl1 = new FanControl(&settings.isEnabled1, &settings.onTime1, &settings.onPercentage1, 25);  // GPIO 25
-    fanControl2 = new FanControl(&settings.isEnabled2, &settings.onTime2, &settings.onPercentage2, 26);  // GPIO 26
-    fanControl3 = new FanControl(&settings.isEnabled3, &settings.onTime3, &settings.onPercentage3, 27);  // GPIO 27
-    fanControl4 = new FanControl(&settings.isEnabled4, &settings.onTime4, &settings.onPercentage4, 14);  // GPIO 14
+    fanControl1 = new FanControl(&settings.isEnabled1, &settings.onTime1, &settings.onPercentage1, 15);  // GPIO 25
+    fanControl2 = new FanControl(&settings.isEnabled2, &settings.onTime2, &settings.onPercentage2, 2);  // GPIO 26
+    fanControl3 = new FanControl(&settings.isEnabled3, &settings.onTime3, &settings.onPercentage3, 0);  // GPIO 27
+    fanControl4 = new FanControl(&settings.isEnabled4, &settings.onTime4, &settings.onPercentage4, 4);  // GPIO 14
 
     // Initialisierung der FanControl-Instanzen
     fanControl1->initialize();
