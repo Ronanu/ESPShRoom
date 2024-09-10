7ab280111a927a90edf55e9fb8925a764abb10c6
Fx <felixrodlsberger@gmail.com>
2024-09-10 17:28:27 +0200

    6 min runtime and going

--- ESP_ShRoom/src/main.cpp
+++ ESP_ShRoom/src/main.cpp
@@ -12,6 +12,7 @@
 
 // Sensor-Instanzen
 SHT20Sensor sht20_1(18, 19, Wire);  // Sensor 1 an Pin 18, 19
+//SHT20Sensor sht20_2(21, 22, Wire1);  // Sensor 2 an Pin 21, 22
 
 // Globales Settings-Objekt
 Settings settings;
@@ -24,42 +25,34 @@ const char* password = "12345678";
 
 WebServer server(80);
 
+
 // FanControl-Instanzen für vier Lüfter
 FanControl* fanControl1;
 FanControl* fanControl2;
 FanControl* fanControl3;
 FanControl* fanControl4;
 
-TemperatureController tempcontr1(&settings.targetTemperature, &settings.hysteresis, &settings.isEnabled3);
+TemperatureController tempcontr1(& settings.targetTemperature, & settings.hysteresis, & settings.isEnabled3);
 
-// Task-Handle für den WebServer
-TaskHandle_t WebServerTaskHandle;
 
-// Mutex-Handle für die Synchronisierung
-SemaphoreHandle_t settingsMutex;
 
-// WebServer-Task
-void handleWebServer(void* pvParameters) {
-    for (;;) {
-        // Mutex sperren, bevor auf `settings` zugegriffen wird
-        if (xSemaphoreTake(settingsMutex, portMAX_DELAY)) {
-            server.handleClient();  // Anfragen bearbeiten
-            xSemaphoreGive(settingsMutex);  // Mutex freigeben
-        }
-        delay(10);  // Kurze Pause, um die CPU nicht zu überlasten
-    }
-}
+
 
 void setup() {
     Serial.begin(115200);
 
     // Initialisierung der Sensoren
     if (!sht20_1.begin()) {
-        Serial.println("Maximale Anzahl von Versuchen erreicht, Sensor 1 konnte nicht verbunden werden.");
-    } else {
-        Serial.println("Sensor 1 erfolgreich initialisiert.");
-    }
-
+            Serial.println("Maximale Anzahl von Versuchen erreicht, Sensor 1 konnte nicht verbunden werden.");
+        } else {
+            Serial.println("Sensor 1 erfolgreich initialisiert.");
+        }
+    //if (!sht20_2.begin()) {
+    //        Serial.println("Maximale Anzahl von Versuchen erreicht, Sensor 2 konnte nicht verbunden werden.");
+    //    } else {
+    //        Serial.println("Sensor 2 erfolgreich initialisiert.");
+    //   }
+    
     // Konfiguration des ESP32 als Access Point
     WiFi.softAP(ssid, password);
     Serial.println("Access Point gestartet");
@@ -76,58 +69,44 @@ void setup() {
     server.on("/setTime", [&]() { handleSetTimePage(&settings, server); });
     server.on("/updateTime", [&]() { handleSetTime(&settings, server, &preferences); });
     server.on("/set_values", [&]() { handleSetValuesPage(&settings, server); });
+    // Registriere den Endpunkt für die Datenaktualisierung
     server.on("/updateData", [&]() { handleUpdateData(&settings, server); });
     server.on("/update_values", [&]() { handleSetValues(&settings, server, &preferences); });
     server.begin();
     Serial.println("Webserver gestartet");
 
     // FanControl-Instanzen erstellen und initialisieren
-    fanControl1 = new FanControl(&settings.isEnabled1, &settings.onTime1, &settings.onPercentage1, 25);
-    fanControl2 = new FanControl(&settings.isEnabled2, &settings.onTime2, &settings.onPercentage2, 17);
-    fanControl3 = new FanControl(&settings.isEnabled3, &settings.onTime3, &settings.onPercentage3, 32);
-    fanControl4 = new FanControl(&settings.isEnabled4, &settings.onTime4, &settings.onPercentage4, 33);
+    fanControl1 = new FanControl(& settings.isEnabled1, & settings.onTime1, & settings.onPercentage1, 25);  // GPIO 15
+    fanControl2 = new FanControl(& settings.isEnabled2, & settings.onTime2, & settings.onPercentage2, 17);  // GPIO 2
+    fanControl3 = new FanControl(& settings.isEnabled3, & settings.onTime3, & settings.onPercentage3, 32);  // GPIO 0
+    fanControl4 = new FanControl(& settings.isEnabled4, & settings.onTime4, & settings.onPercentage4, 33);  // GPIO 4
 
+    // Initialisierung der FanControl-Instanzen
     fanControl1->initialize();
     fanControl2->initialize();
     fanControl3->initialize();
     fanControl4->initialize();
-
-    // Mutex für Settings erstellen
-    settingsMutex = xSemaphoreCreateMutex();
-
-    // WebServer-Task auf Core 0 starten
-    xTaskCreatePinnedToCore(
-        handleWebServer,
-        "WebServerTask",
-        10000,
-        NULL,
-        1,
-        &WebServerTaskHandle,
-        0  // Core 0
-    );
 }
 
 void loop() {
     int start = millis();
+    //Sensordaten aktualisieren
+    settings.temperature1 = sht20_1.getTemperature();
+    //settings.temperature2 = sht20_2.getTemperature();
+    settings.humidity1 = sht20_1.getHumidity();
+    //settings.humidity2 = sht20_2.getHumidity();
+    tempcontr1.update(settings.temperature1);
+    // Uhrzeit aktualisieren
+    updateTime(&settings, &preferences);    
+    // Webserver anfragen bearbeiten
+    server.handleClient();
 
-    // Sensordaten aktualisieren
-    if (xSemaphoreTake(settingsMutex, portMAX_DELAY)) {
-        settings.temperature1 = sht20_1.getTemperature();
-        settings.humidity1 = sht20_1.getHumidity();
-        tempcontr1.update(settings.temperature1);
-        xSemaphoreGive(settingsMutex);  // Mutex freigeben
-    }
+    // Aktualisiere den Lüfterstatus für alle vier Lüfter
+    fanControl1->update();
+    fanControl2->update();
+    fanControl3->update();
+    fanControl4->update();
 
-    // Lüfterstatus aktualisieren
-    if (xSemaphoreTake(settingsMutex, portMAX_DELAY)) {
-        fanControl1->update();
-        fanControl2->update();
-        fanControl3->update();
-        fanControl4->update();
-        xSemaphoreGive(settingsMutex);  // Mutex freigeben
-    }
-
-    // Zeit in der Schleife anzeigen
     Serial.println("\n\n\nLoop time: " + String(millis() - start) + " ms");
     printSettings(settings);
 }
